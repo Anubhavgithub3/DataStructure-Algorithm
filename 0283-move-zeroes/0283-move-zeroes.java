626cb264aa94c597931296f9649290f94f7d4e34
ANUBHAV KUMAR <anubhav9798kumar7033@gmail.com>
2026-09-10 00:06:52 +0530

    Time: 2 ms (91.76%), Space: 48 MB (20.19%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,26 +1,50 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        // --- optimalwa code
         int n = nums.length;
-        int count = 0;
-        for(int i =0; i < n; i++){
-            if (nums[i] == 0){
-                count++;
+        int j = -1;
+        for(int i = 0 ; i < n;i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
             }
         }
-        int[] temp = new int[(n - count)];
-
-        int j =0;
-        for (int i = 0; i < n;i++){
-            if( nums[i] != 0){
-                temp[j] = nums[i];
+        if(j == -1){
+            return;
+        }
+        for(int i = j+1;i < n;i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 j++;
             }
         }
-        for(int i = 0; i < n-count; i++){
-            nums[i] = temp[i];
-        }
-        for(int i = n-count;i<n;i++){
-            nums[i] = 0;
-        }
+        
+        
+        
+        // ------Brute force
+        // int n = nums.length;
+        // int count = 0;
+        // for(int i =0; i < n; i++){
+        //     if (nums[i] == 0){
+        //         count++;
+        //     }
+        // }
+        // int[] temp = new int[(n - count)];
+
+        // int j =0;
+        // for (int i = 0; i < n;i++){
+        //     if( nums[i] != 0){
+        //         temp[j] = nums[i];
+        //         j++;
+        //     }
+        // }
+        // for(int i = 0; i < n-count; i++){
+        //     nums[i] = temp[i];
+        // }
+        // for(int i = n-count;i<n;i++){
+        //     nums[i] = 0;
+        // }
     }
 }
